923d9ec3a2d16e4905ef2617fee3ee28f330739b
gtauro <gtauro@nd.edu>
2018-10-30 22:48:25 -0400

    Added comments to exercise script

--- scriptEx08.R
+++ scriptEx08.R
@@ -3,56 +3,56 @@ rm(list = ls())  # clears global environment
 setwd("~/Biocomp-Fall2018-181026-Exercise8")  # sets working directory to exercise file
 
 # QUESTION 1
-theBigGame <- read.delim("UWvMSU_1-22-13.txt", header = TRUE, sep = "\t", dec = ".")
+theBigGame <- read.delim("UWvMSU_1-22-13.txt", header = TRUE, sep = "\t", dec = ".")  # reads in text file for basketball data as a dataset
 
-badgerBoxScore = matrix()
-badgerCounter = 0
-spartanBoxScore = matrix()
-spartanCounter = 0
+badgerBoxScore = matrix()  # creates a matrix for the storage of UW scores
+badgerCounter = 0  # works as a counter for the UW score being stored into matrix
+spartanBoxScore = matrix()  # creates a matrix for the storage of MSU scores
+spartanCounter = 0  # works as a counter for the MSU score being stored into matrix
 
-for (score in 1:nrow(theBigGame)) {
-  if (theBigGame[score, 2] == "UW"){
+for (score in 1:nrow(theBigGame)) {  # begins for loop that repeats itself over the number of rows present in theBigGame
+  if (theBigGame[score, 2] == "UW"){  # sets conditional for UW -  if UW is present in "team" column, score is added to badgerCounter which is then inputted into badgerBoxScore
     badgerCounter = badgerCounter + theBigGame[score,3]
     badgerBoxScore[score] = badgerCounter
     spartanBoxScore[score] = spartanCounter
   } else {
-    spartanCounter = spartanCounter + theBigGame[score,3]
+    spartanCounter = spartanCounter + theBigGame[score,3]  # sets conditional for MSU -  if MSU is present in "team" column, score is added to spartanCounter which is then inputted into spartanBoxScore
     spartanBoxScore[score] = spartanCounter
     badgerBoxScore[score] = badgerCounter
   }
 }
 
-plot(theBigGame$time, badgerBoxScore, type = "l", lty = 1, col = "red", xlab = "Time", ylab = "Score", main = "1/22/13 - MSU @ UW")
-lines(theBigGame$time, spartanBoxScore, lty = 2, col = "black")
-legend(5, 45, legend = c("UW", "MSU"), col = c("red", "black"), lty = 1:2)
+plot(theBigGame$time, badgerBoxScore, type = "l", lty = 1, col = "red", xlab = "Time", ylab = "Score", main = "1/22/13 - MSU @ UW")  # plots badgerBoxScore on a line graph
+lines(theBigGame$time, spartanBoxScore, lty = 2, col = "black")  # adds spartanBoxScore as a line to the existing line graph
+legend(5, 45, legend = c("UW", "MSU"), col = c("red", "black"), lty = 1:2)  # creates a legend to help distinguish MSU from UW lines
 
 # QUESTION 2
-randomNum = sample(1:100, 1, replace = T)
+randomNum = sample(1:100, 1, replace = T)  # generates a random number between 1 and 100 and sets it as variable randomNum
 
-guess <- function(){
-  integer <- readline(prompt="Guess: ")
-  response <- as.integer(integer)
+guess <- function(){  # creates function for user input and defines input as an integer
+  integer <- readline(prompt="Guess: ")  # user input as variable integer
+  response <- as.integer(integer)  # variable integer converted into actual integer
   
-  return(response)
+  return(response)  # returns generated integer
 }
 
-guessCheck <- function(response){
-  higher <- "\nHigher"
-  lower <- "\nLower"
-  congrats <- "\nCongratulations, human! You've won this time."
+guessCheck <- function(response){  # creates function that checks user input to see if randomNum is higher or lower
+  higher <- "\nHigher"  # computer message if higher
+  lower <- "\nLower"  # computer message if lower
+  congrats <- "\nCongratulations, human! You've won this time."  # computer message if correct
   
-  while (response != randomNum){
-    if (response < randomNum){
+  while (response != randomNum){  # conditional loop for initiating higher or lower messages
+    if (response < randomNum){  # if less than randomNum, system says that randomNum is higher
       cat(higher)
       response <- as.integer(guess())
-    } else {
+    } else {  # if higher than randomNum, system says that randomNum is lower
       cat(lower)
       response <- as.integer(guess())
     }
   }
   
-  return(cat(congrats))
+  return(cat(congrats))  # returns congratulations message if user input is correct
   
 }
 
-paste(cat("\nHello, mortal. Let's play a game. I'm thinking of a number between 1-100. If you can guess it, you win."), guessCheck(guess()), "Exercise Complete.")
+paste(cat("\nHello, mortal. Let's play a game. I'm thinking of a number between 1-100. If you can guess it, you win."), guessCheck(guess()), "Exercise Complete.")  # initializes computer message, nests guess() function in guessCheck() function to start user input cycle, ends with exercise completion message
